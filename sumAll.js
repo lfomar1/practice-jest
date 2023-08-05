8b264cbd5b88907467892726dd50744ce5255db9
Lucas <107668539+lfomar1@users.noreply.github.com>
2023-08-04 23:43:44 -0400

    Playing around with unit testing

---
+++ sumAll.js
@@ -0,0 +1,4 @@
+function sumAll(a, b) {
+  return a + b;
+}
+module.exports = sumAll;
---
+++ sumAll.test.js
@@ -0,0 +1,5 @@
+const sumAll = require("./sumAll");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sumAll(1, 2)).toBe(3);
+});
